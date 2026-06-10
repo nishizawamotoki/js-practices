5c49107ee5840dc08bdb65c6938ae4f934541d46
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-06-10 13:35:29 +0900

    FizzBuzz 問題を解くプログラムを実装した

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+for (var n = 1; n <= 20; n++) {
+  if (n % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (n % 3 === 0) {
+    console.log("Fizz");
+  } else if (n % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(String(n));
+  }
+}
