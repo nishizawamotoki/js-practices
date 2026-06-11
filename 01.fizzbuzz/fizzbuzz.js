778a62c017139a1b634938e890b24a253dcda943
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-06-11 12:30:38 +0900

    同値演算子を使うように修正した

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 for (let n = 1; n <= 20; n++) {
-  if (n % 3 === 0 && n % 5 == 0) {
+  if (n % 3 === 0 && n % 5 === 0) {
     console.log("FizzBuzz");
   } else if (n % 3 === 0) {
     console.log("Fizz");
